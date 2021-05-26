ba2a9e7e1149e4328d713aecf2877ad57c222a4b
NITHIN.C <71306445+niths1271@users.noreply.github.com>
2021-05-26 16:01:34 +0530

    Update STUDENT ENROLLMENT SCRIPT.sql

--- STUDENT ENROLLMENT DATABASE/STUDENT ENROLLMENT SCRIPT.sql
+++ STUDENT ENROLLMENT DATABASE/STUDENT ENROLLMENT SCRIPT.sql
@@ -18,25 +18,25 @@ CREATE TABLE TEXT(BOOK_ISBN INT,BOOK_TITLE VARCHAR(30),PUBLISHER VARCHAR(30),AUT
 				  FOREIGN KEY(BOOK_ISBN) REFERENCES COURSE(BOOK_ISBN) ON UPDATE CASCADE);
 
 INSERT INTO STUDENT(REG_NO,SNAME,MAJOR,BDATE) VALUES ('CS01', 'RAM', 'DS', '1986-03-12'),
-													 ('IS02', 'SMITH', 'USP', '1987-12-23'),
-													 ('EC03', 'AHMED', 'SNS', '1985-04-17'),
-													 ('CS03', 'SNEHA', 'DBMS', '1987-01-01'),
-													 ('TC05', 'AKHILA', 'EC', '1986-10-06');
+						     ('IS02', 'SMITH', 'USP', '1987-12-23'),
+					             ('EC03', 'AHMED', 'SNS', '1985-04-17'),
+					             ('CS03', 'SNEHA', 'DBMS', '1987-01-01'),
+						     ('TC05', 'AKHILA', 'EC', '1986-10-06');
 INSERT INTO COURSE(COURSE_ID,CNAME,DEPT) VALUES (11, 'DS', 'CS'),
-												(22, 'USP', 'IS'),
-												(33, 'SNS', 'EC'),
-												(44, 'DBMS', 'CS'),
-												(55, 'EC', 'TC');
+						(22, 'USP', 'IS'),
+						(33, 'SNS', 'EC'),
+						(44, 'DBMS', 'CS'),
+						(55, 'EC', 'TC');
 INSERT INTO ENROLL(REG_NO,COURSE_ID,SEM,MARKS) VALUES ('CS01', 11, 4, 85),
-													  ('IS02', 22, 6, 80),
-													  ('EC03', 33, 2, 80),
-													  ('CS03', 44, 6, 75),
-													  ('TC05', 55, 2, 8);
+						      ('IS02', 22, 6, 80),
+						      ('EC03', 33, 2, 80),
+						      ('CS03', 44, 6, 75),
+						      ('TC05', 55, 2, 8);
 INSERT INTO BOOK_ADOPTION(COURSE_ID,SEM,BOOK_ISBN) VALUES (11,4,1),
                                                           (11,4,2),
-														  (44,6,3),
+							  (44,6,3),
                                                           (44,6,4),
-														  (55,2,5),
+							  (55,2,5),
                                                           (22,6,6),
                                                           (55,2,7);
                                             
